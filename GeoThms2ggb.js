e7a20bea322e363c96464be22dbcb13a1b0ed77e
Zoltán Kovács <zoltan@geogebra.org>
2018-10-03 07:42:16 +0200

    Some cleanup

--- GeoThms2ggb.js
+++ GeoThms2ggb.js
@@ -30,7 +30,7 @@ async function GGBScript2GGBFile(filename, ggbScript) {
   var ggb = await plotter.export64("ggb");
   await plotter.release(); 
   console.log(filename + ": GGBPlotter released");
-  fileDescriptor = await fsOpen(filename, 'w');
+  var fileDescriptor = await fsOpen(filename, 'w');
   var ggbRaw = Buffer.from(ggb, 'base64');
   await fsWrite(fileDescriptor, ggbRaw);
   await fsClose(fileDescriptor);
@@ -60,24 +60,24 @@ con.connect(function(err) {
 async function processQuery (err, result, fields) {
     if (err) throw err;
     var rows = result.length;
-    filesToProcess = 0;
-    filesProcessed = 0;
-    processingTable = true;
     for (var i = 0; i < rows; i++) {
       var gclcScript = result[i].code.toString();
       var filename = result[i].teoId + ".ggb";
       var ggbScript = gclc2ggb(gclcScript);
       if (ggbScript != null) {
         console.log(filename + " is to be processed");
-        filesToProcess++;
         // console.log("gclcScript: " + gclcScript);
         // console.log("ggbScript: " + ggbScript);
         await GGBScript2GGBFile(filename, ggbScript);
+        } else {
+        var gclcFilename = result[i].teoId + ".gcl";
+        var fileDescriptor = await fsOpen(gclcFilename, 'w');
+        await fsWrite(fileDescriptor, gclcScript);
+        await fsClose(fileDescriptor);
         }
       }
     process.exit();
   }
-
 
 function nextLabel() {
   lastLabel++;
@@ -92,232 +92,4 @@ function gclc2ggb(program) {
     console.log(err);
     return null;
     }
-  }
-
-function gclc2ggb_old(program) {
-  var scaling = 30;
-  var proveCommand = "Prove";
-  var lines = program.length;
-  var output = "";
-  for (var i = 0; i < lines; i++) {
-    line = program[i].replace(/[\r]+$/, '');
-    found = false;
-    var result = line.match( /^\s*line\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*$/ );
-    if (result != null) {
-      output += result[1] + "=Line(" + result[2] + "," + result[3] + ")\n";
-      found = true;
-      }
-    if (!found) {
-      result = line.match( /^\s*point\s*([\w]+)\s*(\d+\.?\d*|\.\d+)\s*(\d+\.?\d*|\.\d+)\s*$/ );
-      if (result != null) {
-        output += result[1] + "=(" + (result[2]/scaling) + "," + (result[3]/scaling) + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*intersec(tion)*\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += result[2] + "=Intersect(" + result[3] + "," + result[4] + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*intersec(tion)*2\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += result[2] + "=Intersect(" + result[4] + "," + result[5] + ",1)\n";
-        output += result[3] + "=Intersect(" + result[4] + "," + result[5] + ",2)\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*intersec(tion)*\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += result[2] + "=Intersect(Line(" + result[3] + "," + result[4] + "),Line(" + result[5] + "," + result[6] + "))\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*drawsegment\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += "Segment(" + result[1] + "," + result[2] + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*drawdashsegment\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += "Segment(" + result[1] + "," + result[2] + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*onsegment\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += result[1] + "=Point(Segment(" + result[2] + "," + result[3] + ")," + (Math.random()/10+0.6) + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*online\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += result[1] + "=Point(Line(" + result[2] + "," + result[3] + ")," + (Math.random()/10+0.6) + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*oncircle\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += result[1] + "=Point(Circle(" + result[2] + "," + result[3] + ")," + (Math.random()/2+0.25) + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*midpoint\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += result[1] + "=Midpoint(" + result[2] + "," + result[3] + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*circle\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += result[1] + "=Circle(" + result[2] + "," + result[3] + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*drawcircle\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += "Circle(" + result[1] + "," + result[2] + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*draw(dash)*line\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += "Line(" + result[2] + "," + result[3] + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*foot\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += result[1] + "=Intersect(PerpendicularLine(" + result[2] + "," + result[3] + ")," + result[3] + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*parallel\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += result[1] + "=Line(" + result[2] + "," + result[3] + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*perp(endicular)*\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += result[2] + "=PerpendicularLine(" + result[3] + "," + result[4] + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*med(iatrice)*\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) {
-        output += result[2] + "=PerpendicularBisector(" + result[3] + "," + result[4] + ")\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*prove\s*{\s*collinear\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*}\s*$/ );
-      if (result != null) {
-        output += proveCommand + "(AreCollinear(" + result[1] + "," + result[2] + "," + result[3] + "))\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*prove\s*{\s*perpendicular\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*}\s*$/ );
-      if (result != null) {
-        output += proveCommand + "(ArePerpendicular(Line(" + result[1] + "," + result[2] + "),Line(" + result[3] + "," + result[4] + ")))\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*prove\s*{\s*parallel\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*}\s*$/ );
-      if (result != null) {
-        output += proveCommand + "(AreParallel(Line(" + result[1] + "," + result[2] + "),Line(" + result[3] + "," + result[4] + ")))\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*prove\s*{\s*cyclic\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*([\w]+)\s*}\s*$/ );
-      if (result != null) {
-        output += proveCommand + "(AreConcyclic(" + result[1] + "," + result[2] + "," + result[3] + "," + result[4] + "))\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*prove\s*{\s*equal\s*{\s*segment\s*([\w]+)\s*([\w]+)\s*}\s*{\s*segment\s*([\w]+)\s*([\w]+)\s*}\s*}\s*$/ );
-      if (result != null) {
-        output += proveCommand + "(AreCongruent(Segment(" + result[1] + "," + result[2] + "),Segment(" + result[3] + "," + result[4] + ")))\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*prove\s*{\s*identical\s*([\w]+)\s*([\w]+)\s*}\s*$/ );
-      if (result != null) {
-        output += proveCommand + "(AreEqual(" + result[1] + "," + result[2] + "))\n";
-        found = true;
-        }
-      }
-    if (!found) {
-      result = line.match( /^\s*cmark_([\w]+)\s*([\w]+)\s*$/ );
-      if (result != null) // cmark_... is not yet implemented
-        found = true;
-      }
-    if (!found) {
-      result = line.match( /^\s*prover_timeout\s*(\d+\.?\d*|\.\d+)\s*$/ );
-      if (result != null) // not implemented
-        found = true;
-      }
-    if (!found) {
-      result = line.match( /^\s*dim\s*(\d+\.?\d*|\.\d+)\s*(\d+\.?\d*|\.\d+)\s*$/ );
-      if (result != null) // not implemented
-        found = true;
-      }
-    if (!found) {
-      result = line.match( /^\s*color\s*(\d+\.?\d*|\.\d+)\s*(\d+\.?\d*|\.\d+)\s*(\d+\.?\d*|\.\d+)\s*$/ );
-      if (result != null) // not implemented
-        found = true;
-      }
-    if (!found) {
-      result = line.match( /^\s*area\s*(\d+\.?\d*|\.\d+)\s*(\d+\.?\d*|\.\d+)\s*(\d+\.?\d*|\.\d+)\s*(\d+\.?\d*|\.\d+)\s*$/ );
-      if (result != null) // not implemented
-        found = true;
-      }
-    if (!found) {
-      result = line.match( /^\s*prooflimit\s*(\d+\.?\d*|\.\d+)\s*$/ );
-      if (result != null) // not implemented
-        found = true;
-      }
-    if (!found) {
-      result = line.match( /^\s*prooflevel\s*(\d+\.?\d*|\.\d+)\s*$/ );
-      if (result != null) // not implemented
-        found = true;
-      }
-    if (!found) {
-      result = line.match( /^\s*$/ ); // empty line
-      if (result != null)
-        found = true;
-      }
-    if (!found) {
-      result = line.match( /^\s*%/ ); // comment
-      if (result != null)
-        found = true;
-      }
-    if (!found) {
-      console.log("Implementation required for '" + line + "'");
-      return null; // implementation required
-      }
-    }
-  return output;
   }
